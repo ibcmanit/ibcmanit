f4cf02aed3bccc166c27fd2289afc95592ce7cd7
pratyushlokhande <pratyush3141@gmail.com>
2022-02-08 17:33:29 +0530

    subscription form added

--- js/success-page.js
+++ js/success-page.js
@@ -2,9 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     let tl = new gsap.timeline();
 
-    tl.from(".modal-container", {
-      opacity: 0,
-      y: -100,
+    tl.to(".modal-container", {
+      opacity: 1,
       duration: 0.5,
       ease: "power1.inOut",
     });
